c58272332e0129be2b38bf8a2e7ed79d7b6415e3
tarunmunjal <tmunjal@email.com>
2017-07-25 10:45:53 -0400

    invoke linux commands from windows powershell using Posh-ssh

---
+++ Invoke-LinuxCommands.psm1
@@ -0,0 +1,56 @@
+﻿Function Invoke-LinuxCommands
+{
+[cmdletbinding()]
+    Param(
+    [Parameter(Mandatory=$true)]
+    $Servers = @(),
+    [Parameter(Mandatory=$true)]
+    $Commands = @(),
+    [Parameter(Mandatory=$true)]
+    [System.Management.Automation.CredentialAttribute()]$Credentials
+    )   
+    try
+    {
+        Import-module Posh-ssh -ErrorAction Stop
+    }
+    catch
+    {
+        Throw "Unable to import POSH-SSH module. Please make sure the module is installed. Here is a link to the module https://github.com/darkoperator/Posh-SSH"
+    }
+    $Report = @()
+    $CommandBlock = @'
+    Param($server,$commands,$Credentials)
+    $SSHSession = New-SSHSession -ComputerName $server -Credential $Credentials -AcceptKey -ConnectionTimeout 60
+    if($SSHSession.connected)
+    {
+        [regex]$ExpectedString = "\][$#] $"        
+        foreach($command in $commands)
+        {
+            $ShellStream = New-SSHShellStream -SSHSession $SSHSession 
+            $ShellStream.WriteLine($command)
+            $ShellStream.Expect($ExpectedString)
+            $ShellStream.Read()
+            $ShellStream.Dispose()
+        }
+        $SSHSession.Disconnect()
+    }
+    
+'@
+    $Scriptblock = [scriptblock]::Create($CommandBlock)
+    $Jobs = @()
+    foreach($server in $servers)
+    {
+        $jobs += Start-Job -Name $server -ScriptBlock $Scriptblock -ArgumentList $server,$commands,$Credentials 
+    }
+    Get-Job | Wait-Job | Out-Null
+    foreach($job in $Jobs)
+    {
+        $NewReportObject = New-Object psobject -Property @{
+            Server = $Job | select -ExpandProperty Name
+            Report = $job | Receive-Job
+        }
+        $Report += $NewReportObject
+    }
+    Get-Job | Remove-Job
+    $Report
+}
