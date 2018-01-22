3c37774cea8e971c289dd8376a5de99f4abac023
tarunmunjal <tarunmunjal@msn.com>
2018-01-22 12:30:33 -0500

    updated git module to add stream and ssh options

--- Invoke-LinuxCommands.psm1
+++ Invoke-LinuxCommands.psm1
@@ -7,7 +7,11 @@ Function Invoke-LinuxCommands
     [Parameter(Mandatory=$true)]
     $Commands = @(),
     [Parameter(Mandatory=$true)]
-    [System.Management.Automation.CredentialAttribute()]$Credentials
+    [System.Management.Automation.CredentialAttribute()]$Credentials,
+    [Parameter(Mandatory=$false)]
+    [switch]$UseStream,
+    [Parameter(Mandatory=$false)]
+    $CommandTimeOut = 120
     )   
     try
     {
@@ -18,38 +22,85 @@ Function Invoke-LinuxCommands
         Throw "Unable to import POSH-SSH module. Please make sure the module is installed. Here is a link to the module https://github.com/darkoperator/Posh-SSH"
     }
     $Report = @()
-    $CommandBlock = @'
-    Param($server,$commands,$Credentials)
+    $StreamBlock = @'
+    Param($server,$commands,$Credentials,$CommandTimeOut)
     $SSHSession = New-SSHSession -ComputerName $server -Credential $Credentials -AcceptKey -ConnectionTimeout 60
     if($SSHSession.connected)
     {
-        [regex]$ExpectedString = "\][$#] $"        
+        #[regex]$ExpectedString = "\]\$ $"
+        [regex]$ExpectedString = "\][\$\#] $" 
+        $ShellStream = New-SSHShellStream -SSHSession $SSHSession 
         foreach($command in $commands)
         {
-            $ShellStream = New-SSHShellStream -SSHSession $SSHSession 
+            $timeSpan = New-TimeSpan -Seconds $CommandTimeOut
             $ShellStream.WriteLine($command)
-            $ShellStream.Expect($ExpectedString)
+            $ShellStream.Expect($ExpectedString,$timeSpan)
             $ShellStream.Read()
-            $ShellStream.Dispose()
         }
-        $SSHSession.Disconnect()
+        $ShellStream.Dispose()
+        Get-SSHSession | Remove-SSHSession
     }
-    
+    else
+    {
+        Write-Output "Unable to connect to $server"
+        Throw "Unable to connect to $server"
+    }  
 '@
+    $InvokeBlock = @'
+    Param($server,$commands,$Credentials,$CommandTimeOut)
+    $SSHSession = New-SSHSession -ComputerName $server -Credential $Credentials -AcceptKey -ConnectionTimeout 60
+    if($SSHSession.connected)
+    {
+        $CommandOutput = @()
+        foreach($command in $commands)
+        {
+            $CommandOutput += Invoke-SSHCommand -Command $command -SSHSession $SSHSession -TimeOut $CommandTimeOut
+        }
+        Get-SSHSession | Remove-SSHSession
+    }
+    else
+    {
+        $CommandOutput = New-Object -TypeName psobject -Property @{output = "Unable to connect to $server"}
+        $CommandOutput
+        Throw "Unable to connect to $server"
+    }
+    $CommandOutput
+'@
+    
+    if($UseStream.IsPresent)
+    {
+        $CommandBlock = $StreamBlock
+    }
+    else
+    {
+        $CommandBlock = $InvokeBlock
+    }
+    $SudoCheck = $Commands | ?{$_ -match "sudo"}
+    if($SudoCheck)
+    {
+        if(!($UseStream.IsPresent))
+        {
+            Write-Warning "one or more of your command(s) contains a sudo. This will not work without stream."
+        }
+    }
     $Scriptblock = [scriptblock]::Create($CommandBlock)
     $Jobs = @()
+    
     foreach($server in $servers)
     {
-        $jobs += Start-Job -Name $server -ScriptBlock $Scriptblock -ArgumentList $server,$commands,$Credentials 
+        $Jobs += Start-Job -Name $server -ScriptBlock $Scriptblock -ArgumentList $server,$commands,$Credentials,$CommandTimeOut
     }
     Get-Job | Wait-Job | Out-Null
-    foreach($job in $Jobs)
+    foreach($Job in $Jobs)
     {
-        $NewReportObject = New-Object psobject -Property @{
-            Server = $Job | select -ExpandProperty Name
-            Report = $job | Receive-Job
+        if($UseStream.IsPresent)
+        {
+            $Report += $Job | select @{N='Server';E={$_.Name}},@{N='Report';E={$Job | Receive-job}}
         }
-        $Report += $NewReportObject
+        else
+        {
+            $Report += $Job | select @{N='Server';E={$_.Name}},@{N='Report';E={($Job | Receive-job).output}}
+        }
     }
     Get-Job | Remove-Job
     $Report
