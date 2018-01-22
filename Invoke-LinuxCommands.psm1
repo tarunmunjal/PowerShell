73f8391c9de4f94f92c95e07eb3f07405d2dd345
tarunmunjal <tarunmunjal@msn.com>
2018-01-22 12:34:57 -0500

    updated invoke-linuxcommands

--- Invoke-LinuxCommands.psm1
+++ Invoke-LinuxCommands.psm1
@@ -6,12 +6,10 @@ Function Invoke-LinuxCommands
     $Servers = @(),
     [Parameter(Mandatory=$true)]
     $Commands = @(),
+    [Parameter(Mandatory=$false)]
+    [int16]$WaitTimeForEachCommand,
     [Parameter(Mandatory=$true)]
-    [System.Management.Automation.CredentialAttribute()]$Credentials,
-    [Parameter(Mandatory=$false)]
-    [switch]$UseStream,
-    [Parameter(Mandatory=$false)]
-    $CommandTimeOut = 120
+    [System.Management.Automation.CredentialAttribute()]$Credentials
     )   
     try
     {
@@ -22,9 +20,17 @@ Function Invoke-LinuxCommands
         Throw "Unable to import POSH-SSH module. Please make sure the module is installed. Here is a link to the module https://github.com/darkoperator/Posh-SSH"
     }
     $Report = @()
-    $StreamBlock = @'
-    Param($server,$commands,$Credentials,$CommandTimeOut)
-    $SSHSession = New-SSHSession -ComputerName $server -Credential $Credentials -AcceptKey -ConnectionTimeout 60
+    $CommandBlock = @'
+    Param($server,$commands,$Credentials,$WaitTimeForEachCommand)
+    try
+    {
+        $SSHSession = New-SSHSession -ComputerName $server -Credential $Credentials -AcceptKey -ConnectionTimeout 60
+    }
+    catch
+    {
+        Write-Error $_.exception.message
+        Throw "Could not establish ssh connection to $server"
+    }
     if($SSHSession.connected)
     {
         #[regex]$ExpectedString = "\]\$ $"
@@ -32,75 +38,37 @@ Function Invoke-LinuxCommands
         $ShellStream = New-SSHShellStream -SSHSession $SSHSession 
         foreach($command in $commands)
         {
-            $timeSpan = New-TimeSpan -Seconds $CommandTimeOut
             $ShellStream.WriteLine($command)
-            $ShellStream.Expect($ExpectedString,$timeSpan)
+            Write-host "Waiting for expected string"
+            if($WaitTimeForEachCommand)
+            {
+                $timeSpan = New-TimeSpan -Seconds $WaitTimeForEachCommand
+                $ShellStream.Expect($ExpectedString,$timeSpan)
+            }
+            else
+            {
+                $ShellStream.Expect($ExpectedString)
+            }           
             $ShellStream.Read()
         }
         $ShellStream.Dispose()
         Get-SSHSession | Remove-SSHSession
     }
-    else
-    {
-        Write-Output "Unable to connect to $server"
-        Throw "Unable to connect to $server"
-    }  
 '@
-    $InvokeBlock = @'
-    Param($server,$commands,$Credentials,$CommandTimeOut)
-    $SSHSession = New-SSHSession -ComputerName $server -Credential $Credentials -AcceptKey -ConnectionTimeout 60
-    if($SSHSession.connected)
-    {
-        $CommandOutput = @()
-        foreach($command in $commands)
-        {
-            $CommandOutput += Invoke-SSHCommand -Command $command -SSHSession $SSHSession -TimeOut $CommandTimeOut
-        }
-        Get-SSHSession | Remove-SSHSession
-    }
-    else
-    {
-        $CommandOutput = New-Object -TypeName psobject -Property @{output = "Unable to connect to $server"}
-        $CommandOutput
-        Throw "Unable to connect to $server"
-    }
-    $CommandOutput
-'@
-    
-    if($UseStream.IsPresent)
-    {
-        $CommandBlock = $StreamBlock
-    }
-    else
-    {
-        $CommandBlock = $InvokeBlock
-    }
-    $SudoCheck = $Commands | ?{$_ -match "sudo"}
-    if($SudoCheck)
-    {
-        if(!($UseStream.IsPresent))
-        {
-            Write-Warning "one or more of your command(s) contains a sudo. This will not work without stream."
-        }
-    }
     $Scriptblock = [scriptblock]::Create($CommandBlock)
     $Jobs = @()
-    
     foreach($server in $servers)
     {
-        $Jobs += Start-Job -Name $server -ScriptBlock $Scriptblock -ArgumentList $server,$commands,$Credentials,$CommandTimeOut
+        $jobs += Start-Job -Name $server -ScriptBlock $Scriptblock -ArgumentList $server,$commands,$Credentials,$WaitTimeForEachCommand 
     }
     Get-Job | Wait-Job | Out-Null
-    foreach($Job in $Jobs)
+    foreach($job in $Jobs)
     {
-        if($UseStream.IsPresent)
-        {
-            $Report += $Job | select @{N='Server';E={$_.Name}},@{N='Report';E={$Job | Receive-job}}
+        $NewReportObject = New-Object psobject -Property @{
+            Server = $Job | select -ExpandProperty Name
+            Report = $job | Receive-Job
         }
-        else
-        {
-            $Report += $Job | select @{N='Server';E={$_.Name}},@{N='Report';E={($Job | Receive-job).output}}
-        }
+        $Report += $NewReportObject
     }
     Get-Job | Remove-Job
     $Report
